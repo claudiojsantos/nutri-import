58630f2c7fc5605e40b2adf7bad389508c9da70a
Cláudio Santos <claudio@sistnet.com.br>
2023-09-23 17:01:10 -0300

    create resource products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  defaults format: :json do
+    resources :products, only: [:index, :show, :update, :delete]
+  end
 end
