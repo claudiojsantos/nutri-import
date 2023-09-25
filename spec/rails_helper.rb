4304596a19f9e88aedc27f349a5a4645e36d0509
Cláudio Santos <claudio@sistnet.com.br>
2023-09-25 09:42:31 -0300

    configure rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
+require 'simplecov'
+
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   config.include Mongoid::Matchers, type: :model
   config.use_active_record = false
   config.infer_spec_type_from_file_location!
