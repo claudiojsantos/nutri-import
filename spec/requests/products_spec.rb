40dfb989fbeebb668872d663fbed528020d7aeeb
Cláudio Santos <claudio@sistnet.com.br>
2023-09-27 21:05:55 -0300

    create test requests DELETE from product

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -40,16 +40,33 @@ RSpec.describe 'Products', type: :request do
     end
   end
 
+  describe 'DELETE /products/:code' do
+    let!(:product) { create(:product, code: '123') }
+    let(:url) { "/products/#{product.code}" }
+
+    context 'when the product exists' do
+      it 'moves the product to trash' do
+        delete url, params: { code: product.code }
+
+        product.reload
+        expect(product.status).to eq('trash')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when the product does not exist' do
+      it 'returns an error' do
+        delete '/products/nonexistent'
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to include('Produto não encontrado')
+      end
+    end
+  end
+
   # describe 'GET /update' do
   #   it 'returns http success' do
   #     get '/products/update'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe 'GET /delete' do
-  #   it 'returns http success' do
-  #     get '/products/delete'
   #     expect(response).to have_http_status(:success)
   #   end
   # end
