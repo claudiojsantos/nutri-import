be5e53cd0cef4e13fb5aae861a7e44e958265ca4
Cláudio Santos <claudio@sistnet.com.br>
2023-09-28 16:36:40 -0300

    added route sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   defaults format: :json do
     get '/', to: 'health_checks#health'
-
     resources :products, only: %i[index update delete]
-
     get 'products/:code', to: 'products#show'
     delete 'products/:code', to: 'products#delete'
   end
