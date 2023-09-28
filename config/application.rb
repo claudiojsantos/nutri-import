c58e25b96fd0d5e6d97f565bce3f6fd5db9fc8e5
Cláudio Santos <claudio@sistnet.com.br>
2023-09-28 16:36:04 -0300

    added middleware

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Importer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
