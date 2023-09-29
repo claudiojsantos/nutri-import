a1d0c2aee1425f2f6bbd93a8e848622244504815
Cláudio Santos <claudio@sistnet.com.br>
2023-09-29 02:37:20 -0300

    added data in CronHistory collection

--- app/workers/enqueue_import_jobs_worker.rb
+++ app/workers/enqueue_import_jobs_worker.rb
@@ -2,12 +2,27 @@ class EnqueueImportJobsWorker
   include Sidekiq::Worker
 
   def perform
+    @start_cron = DateTime.now
+    @memory_before = `ps -o rss= -p #{Process.pid}`.to_i
     filenames = Workers::FetchFileUrlsService.call
 
-    filenames.each do |filename|
-      url = "https://challenges.coode.sh/food/data/json/#{filename}"
+    @time_elapsed = Benchmark.realtime do
+      filenames.each do |filename|
+        url = "https://challenges.coode.sh/food/data/json/#{filename}"
 
-      ImportProductsWorker.perform_async(url)
+        ImportProductsWorker.perform_async(url)
+      end
     end
+
+    @memory_after = `ps -o rss= -p #{Process.pid}`.to_i
+    @finish_cron = DateTime.now
+
+    CronHistory.create(
+      start_cron: @start_cron,
+      finish_cron: @finish_cron,
+      memory_before: @memory_before,
+      memory_after: @memory_after,
+      time_elapsed: @time_elapsed
+    )
   end
 end
