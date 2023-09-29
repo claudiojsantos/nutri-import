503e34a5080a60a1b2c58c982e4a2451af1438d3
Cláudio Santos <claudio@sistnet.com.br>
2023-09-28 21:58:56 -0300

    created folder and moved file

---
+++ app/services/workers/fetch_file_urls_service.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+require 'json'
+
+class Workers::FetchFileUrlsService
+  API_URL = 'https://challenges.coode.sh/food/data/json/index.txt'.freeze
+
+  def self.call
+    uri = URI(API_URL)
+    response = Net::HTTP.get(uri)
+
+    response.split("\n").select { |url| url.end_with?('.json.gz') }
+  rescue StandardError => e
+    Rails.logger.error("Error fetching file URLs: #{e.message}")
+    []
+  end
+end
