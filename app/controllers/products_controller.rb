2315d20a5d09874a364a5aca46d3682883f60b5a
Cláudio Santos <claudio@sistnet.com.br>
2023-09-28 22:28:50 -0300

    added update route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,19 @@ class ProductsController < ApplicationController
     @product = Product.find_by(code: params[:code])
   end
 
-  def update; end
+  def update
+    @product = Product.find_by(code: params[:code])
+
+    return render json: { errors: 'Produto inexistente' }, status: :not_found if @product.nil?
+
+    @service = Product::UpdateService.new(@product, product_params)
+
+    if @service.call
+      render json: { message: 'Produto Atualizado' }, status: :ok
+    else
+      render json: { errors: @service.errors }, status: :unprocessable_entity
+    end
+  end
 
   def delete
     @product = Product.find_by!(code: params[:code])
@@ -24,5 +36,13 @@ class ProductsController < ApplicationController
 
   def paginatable_model
     Product
+  end
+
+  def product_params
+    params.require(:product).permit(
+      :status, :imported_t, :url, :creator, :created_t, :last_modified_t, :product_name, :quantity,
+      :brands, :categories, :labels, :cities, :purchase_places, :stores, :ingredients_text, :traces,
+      :serving_size, :serving_quantity, :nutriscore_score, :nutriscore_grade, :main_category, :image_url
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   defaults format: :json do
     get '/', to: 'health_checks#health'
-    resources :products, only: %i[index update delete]
+    resources :products, only: %i[index delete]
     get 'products/:code', to: 'products#show'
+    put 'products/:code', to: 'products#update'
     delete 'products/:code', to: 'products#delete'
   end
 end
